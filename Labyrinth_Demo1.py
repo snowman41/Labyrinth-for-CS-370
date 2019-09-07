91d87b4f1103f90ca2835140919442ac0a8c0f4e
delimeater <37960551+delimeater@users.noreply.github.com>
2019-09-07 10:00:11 -0400

    Add files via upload
    Has function which gets coordinates of selected tile.

--- Labyrinth_Demo1.py
+++ Labyrinth_Demo1.py
@@ -1,9 +1,12 @@
 #CS370
 #Labyrinth Demo
 
+from Labyrinth_Demo1_Functions import *
 import pygame
 import sys
 import os
+import random
+import glob
 
 singleGridWidth = 100
 singleGridHeight = 100
@@ -11,23 +14,13 @@ gridMargin = 5#Individual grid dimensions
 
 BLACK = (0,0,0)#Colors used in game
 BACKGROUNDCOLOR = (0, 48, 146)
-#gridPosition = [100, 100]
-#x = gridPosition[0]
-#y = gridPosition[1]
 
 numOfColumns = 7
 numOfRows = 7
 
+imagePaths = []
 imageLibrary = {}#Defines dictionary
-
-def getImage(path):#Function which takes in file path and initializes image
-	global imageLibrary
-	image = imageLibrary.get(path)
-	if image == None:
-		canonicalizedPath = path.replace("/", os.sep).replace("\\", os.sep)
-		image = pygame.image.load(canonicalizedPath)
-		imageLibrary[path] = image
-	return image
+alreadyUsedCoords = []#Stores already Used Coordinates
 
 grid = []#Initializes list, used to populate grid
 for row in range(numOfRows):
@@ -41,12 +34,24 @@ for row in range(numOfRows):
 	for column in range(numOfColumns):
 		grid[row].append(0)
 
+os.chdir("C:\pythonProjects\LabyrinthDemo")#Grabs image files from directory
+for file in glob.glob("*.png"):
+    imagePaths.append(file)
+
+
+
+
+
 pygame.init()#initializes pygame
 WINDOW_SIZE = [1280, 960]
 screen = pygame.display.set_mode(WINDOW_SIZE)#Sets the size of the screen
 pygame.display.set_caption("Labyrinth")#Sets title of the screen
 gameOver = False#Loops until user closes out of game
 clock = pygame.time.Clock()#Manages how fast the screen updates
+
+
+
+getTileCoordinates(13)#Gets coordinates for selected tile
 
 while not gameOver:
 	for event in pygame.event.get():#Empties event queue
@@ -55,18 +60,16 @@ while not gameOver:
 
 	screen.fill(BACKGROUNDCOLOR)
 
-	for row in range(numOfRows):#Draws the grid
-		for column in range(numOfColumns):
-			#surface = pygame.surface(100, 100)#Creates surface
+	for row in range(numOfRows):#Draws the grid		
+		for column in range(numOfColumns):			
 			pygame.draw.rect(screen, BLACK, [(gridMargin + singleGridWidth) * column + gridMargin + 250, (gridMargin + singleGridHeight) * row + gridMargin + 110, singleGridWidth, singleGridHeight])  
+			
 
 	screen.blit(getImage("LabyrinthTile1.png"), (255, 115))#Placing fixed tiles
 	screen.blit(getImage("LabyrinthTile7.png"), (885, 115))
 	screen.blit(getImage("LabyrinthTile43.png"), (255, 745))
 	screen.blit(getImage("LabyrinthTile49.png"), (885, 745))
-
-
-
+	
 
 	clock.tick(60)#Sets to 60 frames per second
 
