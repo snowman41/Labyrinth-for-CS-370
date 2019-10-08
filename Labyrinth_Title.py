58a0d0f90ba4cd13ae65297ef133ec3bcd945f49
Katngoode <54676526+Katngoode@users.noreply.github.com>
2019-10-08 10:08:11 -0400

    Add files via upload

--- Labyrinth_Title.py
+++ Labyrinth_Title.py
@@ -1,77 +1,165 @@
 import pygame 
 import sys
 import os
+from enum import Enum
 from pygame.rect import Rect
 from Sprite import *
 
-#Colors used in game
+
+#Global Colors used
 WHITE = (255,255,255)
 GRAY = (120,120,120)
 BLACK = (0, 0, 0)
 
-pygame.init()
-
-#Screen dimensions
-width = 1280
-height = 960
-
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Labyrinth")
-
+#Init clock
 clock = pygame.time.Clock()
-gameover = False
-pygame.mixer.music.load('background.mp3')
-pygame.mixer.music.play(-1)
 
-#Create Quit Game Button
-quit_btn = UIElement(
-	center_position=(640, 600),
-	font_size=30,
-	bg_rgb= BLACK,
-	text_rgb= WHITE,
-	text="Quit",
-	#action = sys.exit(),
-)
+#Main function to track gamestate
+def main():
+	pygame.init()
 
-#Create Start Game Button
-start_btn = UIElement(
-	center_position=(640, 500),
-	font_size=30,
-	bg_rgb= BLACK,
-	text_rgb= WHITE,
-	text="Start Game",
-	#action= (bring player to game screen),
-)
+	#Init GameState to title
+	game_state = GameState.TITLE
 
+	#Screen dimensions
+	width = 1280
+	height = 960
 
-buttons = [start_btn, quit_btn]
+	#Set Screen dimensions and caption
+	screen = pygame.display.set_mode((width, height))
+	pygame.display.set_caption("Labyrinth")
 
-#Main game loop
-while True:
-	
-	mouse_up = False
-	for event in pygame.event.get():
-		if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
-			mouse_up = True
-		elif event.type == pygame.QUIT:
+	#Begin music loop
+	pygame.mixer.music.load('background.mp3')
+	pygame.mixer.music.play(-1)
+
+	#Gamestate switch
+	while True:
+		if game_state == GameState.TITLE:
+			game_state = title_screen(screen)
+
+		if game_state == GameState.GAME:
+			#game_state = play_game(screen)
+			#Quit game for now until we connect the pieces
+			pygame.quit()
+
+		if game_state == GameState.RULES:
+			game_state = rules_screen(screen)
+
+		if game_state == GameState.QUIT:
 			pygame.quit()
 			sys.exit()
+			return
 
-	#Set background color
-	screen.fill(BLACK)
+#Main function of Title Screen
+def title_screen(screen):
 
-	#Set and display Logo
-	logo = pygame.image.load(r'Main_Logo.png')
-	screen.blit(logo, (118, 115))
+	#Create Rules Button
+	rules_btn = UIElement(
+		center_position=(640, 500),
+		font_size=30,
+		bg_rgb= BLACK,
+		text_rgb= WHITE,
+		text="Rules of the Game",
+		action= GameState.RULES,
+	)
+
+	#Create Start Game Button
+	start_btn = UIElement(
+		center_position=(640, 575),
+		font_size=30,
+		bg_rgb= BLACK,
+		text_rgb= WHITE,
+		text= "Start Game",
+		action= GameState.GAME,
+	)
+
+	#Create Quit Game Button
+	quit_btn = UIElement(
+		center_position=(640, 650),
+		font_size=30,
+		bg_rgb= BLACK,
+		text_rgb= WHITE,
+		text= "Quit",
+		action= GameState.QUIT,
+	)
+
+	buttons = [start_btn, quit_btn, rules_btn]
+
+	#Main Title Screen Loop
+	while True:
+		
+		mouse_up = False
+		for event in pygame.event.get():
+			if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+				mouse_up = True
+			elif event.type == pygame.QUIT:
+				pygame.quit()
+				sys.exit()
+
+		#Set background color
+		screen.fill(BLACK)
+
+		#Set and display Logo
+		logo = pygame.image.load(r'Main_Logo.png')
+		screen.blit(logo, (118, 115))
 
 
-	#Draw and update buttons
-	for button in buttons:
-		button.draw(screen)
-		action = button.update(pygame.mouse.get_pos(), mouse_up)
-		if action is not None:
+		#Draw and update buttons
+		for button in buttons:
+			#button.draw(screen)
+			ui_action = button.update(pygame.mouse.get_pos(), mouse_up)
+			if ui_action is not None:
+				return ui_action
 			button.draw(screen)
 
-	#Set to 60 FPS
-	clock.tick(60)
-	pygame.display.flip()
+		#Set to 60 FPS
+		clock.tick(60)
+		pygame.display.flip()
+
+
+#Main function of Rules Screen
+def rules_screen(screen):
+
+	while True:
+
+		#Create Return Button
+		return_btn = UIElement(
+		center_position=(640, 575),
+		font_size=30,
+		bg_rgb= BLACK,
+		text_rgb= WHITE,
+		text="Return to Main Menu",
+		action= GameState.TITLE,
+		)
+
+		mouse_up = False
+		for event in pygame.event.get():
+			if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+				mouse_up = True
+			elif event.type == pygame.QUIT:
+				pygame.quit()
+				sys.exit()
+
+		#Set background color
+		screen.fill(BLACK)
+
+		ui_action = return_btn.update(pygame.mouse.get_pos(), mouse_up)
+		if ui_action is not None:
+			return ui_action
+		return_btn.draw(screen)
+
+		#Set to 60 FPS
+		clock.tick(60)
+		pygame.display.flip()
+
+
+#Enumerated States
+class GameState(Enum) :
+	QUIT = -1
+	TITLE = 0
+	GAME = 1
+	RULES = 2
+
+if __name__ == "__main__":
+	main()
