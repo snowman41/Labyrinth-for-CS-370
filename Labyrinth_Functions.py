3dac86c10893466b8e1a5ec8c7a539e84a60fe5a
delimeater <37960551+delimeater@users.noreply.github.com>
2019-10-27 15:44:44 -0400

    Add files via upload

--- Labyrinth_Functions.py
+++ Labyrinth_Functions.py
@@ -4,8 +4,10 @@ import pygame
 import sys
 import os
 import random
+import numpy 
 from random import shuffle
 import glob
+from Tile import *
 
 def button(display, txt, txtColor, font, fontSize, x, y, w, h, ic, ac):#Function which creates button, (display: source of screen) (txt: what the button says) (txtColor: Color of text) (font: what font the text will be) (x, y: coordinates of button) (w, h: width and height of button) (ic, ac: inactive and active color)
 	mouse = pygame.mouse.get_pos()#Stores position of mouse
@@ -22,17 +24,18 @@ def button(display, txt, txtColor, font, fontSize, x, y, w, h, ic, ac):#Function
 	display.blit(textSurf, textRect)
 
 def text_objects(text, font, color):#Defines text contents, font and color for text
-    textSurface = font.render(text, True, color)
-    return textSurface, textSurface.get_rect()
+	textSurface = font.render(text, True, color)
+	return textSurface, textSurface.get_rect()
 
-def get_tile_coordinates(col, row):#Function which takes in column and row and returns coordinates of tile
-	colRow = tuple([col, row])#Stores col row as coordinate value
+def get_tile_coordinates(row, col):#Function which takes in column and row and returns coordinates of tile
+	colRow = tuple([row, col])#Stores col row as coordinate value
 
 	for c in range(7):
 		for r in range(7):
-			coords = tuple([c*100+(100*3), r*100+100])#Stores coordinate value for current row and column
-			pos = tuple([c, r])
+			coords = tuple([r*100+(100*3), c*100+100])#Stores coordinate value for current row and column
+			pos = tuple([r, c])
 			if pos == colRow:
+				#print(coords)
 				return(coords)
 
 def grab_and_place_arrows(display):#Function which grabs images of arrows and puts them in positions around board
@@ -157,14 +160,39 @@ def grab_and_randomize_tiles():#Function which grabs tiles in specified director
 	tTiles = []#Stores T Tiles
 	randomImagePaths = []#Stores random rotated image paths
 
+	#elbowFiles = ("NorthEast*", "NorthWest*", "*EastSouth*", "SouthWest*")
+	#straightFiles = ("NorthSouth*", "EastWest*")
+	#TFiles = ("NorthEastSouth*", "NorthEastWest*", "NorthSouthWest*", "EastSouthWest*")
+
 	twoDimBoard = [[0 for x in range(7)] for x in range(7)]#Creates matrix to store tiles in random positions
 
 	os.chdir("..\LabyrinthProject")#Grabs image files that are from directory
-	for file in glob.glob("Elbow*"):
+
+	#for files in elbowFiles:
+		#elbowTiles.extend(glob.glob(files))
+	#for files in straightFiles:
+		#straightTiles.extend(glob.glob(files))
+	#for files in TFiles:
+		#tTiles.extend(glob.glob(files))
+	for file in glob.glob("NorthEast.png"):
 		elbowTiles.append(file)
-	for file in glob.glob("Straight*"):
+	for file in glob.glob("NorthWest*"):
+		elbowTiles.append(file)
+	for file in glob.glob("EastSouth.png"):
+		elbowTiles.append(file)
+	for file in glob.glob("SouthWest*"):
+		elbowTiles.append(file)
+	for file in glob.glob("NorthSouth.png"):
 		straightTiles.append(file)
-	for file in glob.glob("T*"):
+	for file in glob.glob("EastWest*"):
+		straightTiles.append(file)
+	for file in glob.glob("NorthEastSouth*"):
+		tTiles.append(file)
+	for file in glob.glob("NorthEastWest*"):
+		tTiles.append(file)
+	for file in glob.glob("NorthSouthWest*"):
+		tTiles.append(file)
+	for file in glob.glob("EastSouthWest*"):
 		tTiles.append(file)
 	for x in range(15):#Number of unfixed elbow tiles
 		randomImagePaths.append(random.choice(elbowTiles))
@@ -173,65 +201,154 @@ def grab_and_randomize_tiles():#Function which grabs tiles in specified director
 	for x in range(6):#Number of unfixed elbow tiles
 		randomImagePaths.append(random.choice(tTiles))
 
-	for c in range(7):
-		for r in range(7):
-			currPos = tuple([c, r])
+	for r in range(7):
+		for c in range(7):
+			currPos = tuple([r, c])
 			if currPos == (0, 1) or currPos == (0, 3) or currPos == (0, 5) or currPos == (1, 0) or currPos == (1, 1) or currPos == (1, 2) or currPos == (1, 3) or currPos == (1, 4) or currPos == (1, 5) or currPos == (1, 6) or currPos == (2, 1) or currPos == (2, 3) or currPos == (2, 5) or currPos == (3, 0) or currPos == (3, 1) or currPos == (3, 2) or currPos == (3, 3) or currPos == (3, 4) or currPos == (3, 5) or currPos == (3, 6) or currPos == (4, 1) or currPos == (4, 3) or currPos == (4, 5) or currPos == (5, 0) or currPos == (5, 1) or currPos == (5, 2) or currPos == (5, 3) or currPos == (5, 4) or currPos == (5, 5) or currPos == (5, 6) or currPos == (6, 1) or currPos == (6, 3) or currPos == (6, 5):#checks if current position is for non fixed tiles
 				imagePath = random.choice(randomImagePaths)
-				twoDimBoard[c][r] = imagePath
+				twoDimBoard[r][c] = imagePath
 				randomImagePaths.remove(imagePath)
 
 	return twoDimBoard
 
 def grab_fixed_tiles():#Function which grabs tiles in specified directory and puts them in fixed positions in matrix
-	elbowTiles = []#Stores elbow tiles
-	tTiles = []#Stores T Tiles
-
 	twoDimBoard = [[0 for x in range(7)] for x in range(7)]#Creates matrix to store tiles in fixed positions
 
 	os.chdir("..\LabyrinthProject")#Grabs image files that are from directory
+
 	for file in glob.glob("Starting*"):
 		twoDimBoard[0][0] = file#Matrix goes by row first and column second
-	for file in glob.glob("ElbowSouthWest*"):
+	for file in glob.glob("SouthWest*"):
 		twoDimBoard[6][0] = file
-	for file in glob.glob("ElbowWestNorth*"):
+	for file in glob.glob("NorthWest*"):
 		twoDimBoard[6][6] = file
-	for file in glob.glob("ElbowNorthEast*"):
+	for file in glob.glob("NorthEast.png"):
 		twoDimBoard[0][6] = file
-	for file in glob.glob("TEastSouthWest*"):
+	for file in glob.glob("EastSouthWest*"):
 		twoDimBoard[2][0] = file
 		twoDimBoard[4][0] = file
 		twoDimBoard[4][2] = file
-	for file in glob.glob("TNorthEastSouth*"):
+	for file in glob.glob("NorthEastSouth*"):
 		twoDimBoard[0][2] = file
 		twoDimBoard[2][2] = file
 		twoDimBoard[0][4] = file
-	for file in glob.glob("TNorthSouthWest*"):
+	for file in glob.glob("NorthSouthWest*"):
 		twoDimBoard[6][2] = file
 		twoDimBoard[4][4] = file
 		twoDimBoard[6][4] = file
-	for file in glob.glob("TWestNorthEast*"):
+	for file in glob.glob("NorthEastWest*"):
 		twoDimBoard[2][4] = file
 		twoDimBoard[2][6] = file
 		twoDimBoard[4][6] = file
 
 	return twoDimBoard
 
+def get_image_filepath(tile):#Takes Tile class instance
+	STARTING01 = pygame.image.load(os.path.join('..\LabyrinthProject', 'Starting01.png')).convert_alpha()#Gets the image path for tile
+	NORTHEAST = pygame.image.load(os.path.join('..\LabyrinthProject', 'NorthEast.png')).convert_alpha()
+	NORTHWEST = pygame.image.load(os.path.join('..\LabyrinthProject', 'NorthWest.png')).convert_alpha()
+	EASTSOUTH = pygame.image.load(os.path.join('..\LabyrinthProject', 'EastSouth.png')).convert_alpha()
+	SOUTHWEST = pygame.image.load(os.path.join('..\LabyrinthProject', 'SouthWest.png')).convert_alpha()
+	NORTHSOUTH = pygame.image.load(os.path.join('..\LabyrinthProject', 'NorthSouth.png')).convert_alpha()
+	EASTWEST = pygame.image.load(os.path.join('..\LabyrinthProject', 'EastWest.png')).convert_alpha()
+	NORTHEASTSOUTH = pygame.image.load(os.path.join('..\LabyrinthProject', 'NorthEastSouth.png')).convert_alpha()
+	NORTHEASTWEST = pygame.image.load(os.path.join('..\LabyrinthProject', 'NorthEastWest.png')).convert_alpha()
+	NORTHSOUTHWEST = pygame.image.load(os.path.join('..\LabyrinthProject', 'NorthSouthWest.png')).convert_alpha()
+	EASTSOUTHWEST = pygame.image.load(os.path.join('..\LabyrinthProject', 'EastSouthWest.png')).convert_alpha()
 
+	tile.image = ("%%%%.png" (tile.North, tile.East, tile.South, tile.West))#Make sure this matches the naming convention for tiles
 
-		
+def rotate_tile_clockwise(tile):
+	tilevalue = tile.north
+	tile.north = tile.west
+	tile.west = tile.south
+	tile.south = tile.east
+	tile.east = tilevalue
 
+def new_tile_initialization():
+	#TILE_ARRAY = allFilePaths
+	#TILE_ARRAY = [[0 for x in range(7)] for x in range(7)]#Creates matrix to store tiles
+	#get_tile_coordinates(0, 0),STARTING01,
+	TILE_ARRAY = numpy.ndarray(shape=(7,7), dtype=object)
+	t0 = Tile(0,1,1,0)#initialize and place all of the tiles that dont move
+	TILE_ARRAY[0][0]=t0
+	t1 = Tile(0,1,1,1)
+	TILE_ARRAY[2][0]=t1
+	t2 = Tile(0,1,1,1)
+	TILE_ARRAY[4][0]=t2
+	t3 = Tile(0,0,1,1)
+	TILE_ARRAY[6][0]=t3
+	t4 = Tile(1,1,1,0)
+	TILE_ARRAY[0][2]=t4
+	t5 = Tile(1,1,1,0)
+	TILE_ARRAY[2][2]=t5
+	t6 = Tile(0,1,1,1)
+	TILE_ARRAY[4][2]=t6
+	t7 = Tile(1,0,1,1)
+	TILE_ARRAY[6][2]=t7
+	t8 = Tile(1,1,1,0)
+	TILE_ARRAY[0][4]=t8
+	t9 = Tile(1,1,0,1)
+	TILE_ARRAY[2][4]=t9
+	t10 = Tile(1,0,1,1)
+	TILE_ARRAY[4][4]=t10
+	t11 = Tile(1,0,1,1)
+	TILE_ARRAY[6][4]=t11
+	t12 = Tile(1,1,0,0)
+	TILE_ARRAY[0][6]=t12
+	t13 = Tile(1,1,0,1)
+	TILE_ARRAY[2][6]=t13
+	t14 = Tile(1,1,0,1)
+	TILE_ARRAY[4][6]=t14
+	t15 = Tile(1,0,0,1)
+	TILE_ARRAY[6][6]=t15
+	t16 = Tile(1,0,1,0)#Straight tiles
+	t17 = Tile(1,0,1,0)
+	t18 = Tile(1,0,1,0)
+	t19 = Tile(1,0,1,0)
+	t20 = Tile(1,0,1,0)
+	t21 = Tile(1,0,1,0)
+	t22 = Tile(1,0,1,0)
+	t23 = Tile(1,0,1,0)
+	t24 = Tile(1,0,1,0)
+	t25 = Tile(1,0,1,0)
+	t26 = Tile(1,0,1,0)
+	t27 = Tile(1,0,1,0)
+	t28 = Tile(1,0,1,0)
+	t29 = Tile(1,1,0,0)#Elbow tiles
+	t30 = Tile(1,1,0,0)
+	t31 = Tile(1,1,0,0)
+	t32 = Tile(1,1,0,0)
+	t33 = Tile(1,1,0,0)
+	t34 = Tile(1,1,0,0)
+	t35 = Tile(1,1,0,0)
+	t36 = Tile(1,1,0,0)
+	t37 = Tile(1,1,0,0)
+	t38 = Tile(1,1,0,0)
+	t39 = Tile(1,1,0,0)
+	t40 = Tile(1,1,0,0)
+	t41 = Tile(1,1,0,0)
+	t42 = Tile(1,1,0,0)
+	t43 = Tile(1,1,0,0)
+	t44 = Tile(1,1,1,0)#T tiles
+	t45 = Tile(1,1,1,0)
+	t46 = Tile(1,1,1,0)
+	t47 = Tile(1,1,1,0)
+	t48 = Tile(1,1,1,0)
+	t49 = Tile(1,1,1,0)#extra tile
 
+	initializationList = [t16,t17,t18,t19,t20,t21,t22,t23,t24,t25,t26,t27,t28,t29,t30,t31,t32,t33,t34,t35,t36,t37,t38,t39,t40,t41,t42,t43,t44,t45,t46,t47,t48,t49]
 
+	for a in initializationList:
+		for b in range(random.randint(0,3)):
+			rotate_tile_clockwise(a)
+	random.shuffle(initializationList)
+	for column in range(7):
+		for row in range(7):
+			#if [row][column] != t0 or [row][column] != t1 or [row][column] != t2 or [row][column] != t3 or [row][column] != t4 or [row][column] != t5 or [row][column] != t6 or [row][column] != t7 or [row][column] != t8 or [row][column] != t9 or [row][column] != t10 or [row][column] != t11 or [row][column] != t12 or [row][column] != t13 or [row][column] != t14 or [row][column] != t15:
+			if TILE_ARRAY[row][column] == None:#This is testing if TILE_ARRAY[row][column] is empty, may have to change logic to work
+				TILE_ARRAY[row][column] = initializationList[0]
+				del initializationList[0]
 
-
-
-
-
-
-
-
-		
-
-
-
+	print(TILE_ARRAY)#shows contents of tile_array
+	return TILE_ARRAY
