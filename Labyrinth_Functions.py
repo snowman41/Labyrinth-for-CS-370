9e9e644c0636607ba4ea7815be68009bd782a35e
Katngoode <54676526+Katngoode@users.noreply.github.com>
2019-11-19 11:57:07 -0500

    Add files via upload

--- Labyrinth_Functions.py
+++ Labyrinth_Functions.py
@@ -1,12 +1,14 @@
+
 #Functions
 
 import pygame
 import sys
 import os
 import random
+import numpy 
 from random import shuffle
 import glob
-import numpy as np
+from Tile import *
 
 def button(display, txt, txtColor, font, fontSize, x, y, w, h, ic, ac):#Function which creates button, (display: source of screen) (txt: what the button says) (txtColor: Color of text) (font: what font the text will be) (x, y: coordinates of button) (w, h: width and height of button) (ic, ac: inactive and active color)
 	mouse = pygame.mouse.get_pos()#Stores position of mouse
@@ -23,17 +25,18 @@ def button(display, txt, txtColor, font, fontSize, x, y, w, h, ic, ac):#Function
 	display.blit(textSurf, textRect)
 
 def text_objects(text, font, color):#Defines text contents, font and color for text
-    textSurface = font.render(text, True, color)
-    return textSurface, textSurface.get_rect()
+	textSurface = font.render(text, True, color)
+	return textSurface, textSurface.get_rect()
 
-def get_tile_coordinates(col, row):#Function which takes in column and row and returns coordinates of tile
-	colRow = tuple([col, row])#Stores col row as coordinate value
+def get_tile_coordinates(row, col):#Function which takes in column and row and returns coordinates of tile
+	colRow = tuple([row, col])#Stores col row as coordinate value
 
 	for c in range(7):
 		for r in range(7):
-			coords = tuple([c*100+(100*3), r*100+100])#Stores coordinate value for current row and column
-			pos = tuple([c, r])
+			coords = tuple([r*100+(100*3), c*100+100])#Stores coordinate value for current row and column
+			pos = tuple([r, c])
 			if pos == colRow:
+				#print(coords)
 				return(coords)
 
 def grab_and_place_arrows(display):#Function which grabs images of arrows and puts them in positions around board
@@ -109,89 +112,210 @@ def grab_and_place_arrows(display):#Function which grabs images of arrows and pu
 	else:
 		display.blit(arrowLeftIC, (1033, 635))
 
+def grab_and_place_movement_keys(display):#Function which grabs images of movement keys and places them in correct positions
+	mouse = pygame.mouse.get_pos()#Stores position of mouse	
+	click = pygame.mouse.get_pressed()
+
+	moveUpIC = pygame.image.load("moveUpIC.png")
+	moveRightIC = pygame.image.load("moveRightIC.png")
+	moveDownIC = pygame.image.load("moveDownIC.png")
+	moveLeftIC = pygame.image.load("moveLeftIC.png")
+	moveUpAC = pygame.image.load("moveUpAC.png")
+	moveRightAC = pygame.image.load("moveRightAC.png")
+	moveDownAC = pygame.image.load("moveDownAC.png")
+	moveLeftAC = pygame.image.load("moveLeftAC.png")	
+
+	if 1110 + 45 > mouse[0] > 1125 and 800 + 60 > mouse[1] > 800:#If mouse clicks, then changes image
+		display.blit(moveUpIC, (1110, 800))
+		if click[0] == 1:
+			display.blit(moveUpAC, (1110, 800))
+	else:
+		display.blit(moveUpIC, (1110, 800))
+
+	if 1110 + 45 > mouse[0] > 1125 and 870 + 60 > mouse[1] > 870:
+		display.blit(moveDownIC, (1110, 870))
+		if click[0] == 1:
+			display.blit(moveDownAC, (1110, 870))
+	else:
+		display.blit(moveDownIC, (1110, 870))
+
+	if 1160 + 45 > mouse[0] > 1160 and 825 + 60 > mouse[1] > 845:
+		display.blit(moveRightIC, (1160, 835))
+		if click[0] == 1:
+			display.blit(moveRightAC, (1160, 835))
+	else:
+		display.blit(moveRightIC, (1160, 835))
+
+	if 1060 + 45 > mouse[0] > 1060 and 825 + 60 > mouse[1] > 845:
+		display.blit(moveLeftIC, (1060, 835))
+		if click[0] == 1:
+			display.blit(moveLeftAC, (1060, 835))
+	else:
+		display.blit(moveLeftIC, (1060, 835))
+
+def get_image_filepath(tile):#Takes Tile class instance
+
+	tile.image = ("%%%%.png" (tile.North, tile.East, tile.South, tile.West))#Make sure this matches the naming convention for tiles
+
+def rotate_tile_clockwise(tile):
+	tilevalue = tile.north
+	tile.north = tile.west
+	tile.west = tile.south
+	tile.south = tile.east
+	tile.east = tilevalue
+
+def new_tile_initialization(TILE_ARRAY):
+	#TILE_ARRAY = allFilePaths
+	#TILE_ARRAY = [[0 for x in range(7)] for x in range(7)]#Creates matrix to store tiles
+	#get_tile_coordinates(0, 0),STARTING01,
+	
+	t0 = Tile(0,1,1,0,0,0)#initialize and place all of the tiles that dont move
+	TILE_ARRAY[0,0]=t0
+	t1 = Tile(0,1,1,1,0,2)
+	TILE_ARRAY[0,2]=t1
+	t2 = Tile(0,1,1,1,0,4)
+	TILE_ARRAY[0,4]=t2
+	t3 = Tile(0,0,1,1,0,6)
+	TILE_ARRAY[0,6]=t3
+	t4 = Tile(1,1,1,0,2,0)
+	TILE_ARRAY[2,0]=t4
+	t5 = Tile(1,1,1,0,2,2)
+	TILE_ARRAY[2,2]=t5
+	t6 = Tile(0,1,1,1,2,4)
+	TILE_ARRAY[2,4]=t6
+	t7 = Tile(1,0,1,1,2,6)
+	TILE_ARRAY[2,6]=t7
+	t8 = Tile(1,1,1,0,4,0)
+	TILE_ARRAY[4,0]=t8
+	t9 = Tile(1,1,0,1,4,2)
+	TILE_ARRAY[4,2]=t9
+	t10 = Tile(1,0,1,1,4,4)
+	TILE_ARRAY[4,4]=t10
+	t11 = Tile(1,0,1,1,4,6)
+	TILE_ARRAY[4,6]=t11
+	t12 = Tile(1,1,0,0,6,0)
+	TILE_ARRAY[6,0]=t12
+	t13 = Tile(1,1,0,1,6,2)
+	TILE_ARRAY[6,2]=t13
+	t14 = Tile(1,1,0,1,6,4)
+	TILE_ARRAY[6,4]=t14
+	t15 = Tile(1,0,0,1,6,6)
+	TILE_ARRAY[6,6]=t15
+	t16 = Tile(1,0,1,0,0,0)#Straight tiles
+	t17 = Tile(1,0,1,0,0,0)
+	t18 = Tile(1,0,1,0,0,0)
+	t19 = Tile(1,0,1,0,0,0)
+	t20 = Tile(1,0,1,0,0,0)
+	t21 = Tile(1,0,1,0,0,0)
+	t22 = Tile(1,0,1,0,0,0)
+	t23 = Tile(1,0,1,0,0,0)
+	t24 = Tile(1,0,1,0,0,0)
+	t25 = Tile(1,0,1,0,0,0)
+	t26 = Tile(1,0,1,0,0,0)
+	t27 = Tile(1,0,1,0,0,0)
+	t28 = Tile(1,0,1,0,0,0)
+	t29 = Tile(1,1,0,0,0,0)#Elbow tiles
+	t30 = Tile(1,1,0,0,0,0)
+	t31 = Tile(1,1,0,0,0,0)
+	t32 = Tile(1,1,0,0,0,0)
+	t33 = Tile(1,1,0,0,0,0)
+	t34 = Tile(1,1,0,0,0,0)
+	t35 = Tile(1,1,0,0,0,0)
+	t36 = Tile(1,1,0,0,0,0)
+	t37 = Tile(1,1,0,0,0,0)
+	t38 = Tile(1,1,0,0,0,0)
+	t39 = Tile(1,1,0,0,0,0)
+	t40 = Tile(1,1,0,0,0,0)
+	t41 = Tile(1,1,0,0,0,0)
+	t42 = Tile(1,1,0,0,0,0)
+	t43 = Tile(1,1,0,0,0,0)
+	t44 = Tile(1,1,0,0,0,0)
+	t45 = Tile(1,1,0,0,0,0)
+	t46 = Tile(1,1,0,0,0,0)
+	t47 = Tile(1,1,1,0,0,0)#T tiles
+	t48 = Tile(1,1,1,0,0,0)
+	t49 = Tile(1,1,1,0,0,0)#extra tile
+
+	initializationList = [t16,t17,t18,t19,t20,t21,t22,t23,t24,t25,t26,t27,t28,t29,t30,t31,t32,t33,t34,t35,t36,t37,t38,t39,t40,t41,t42,t43,t44,t45,t46,t47,t48,t49]
+
+	for a in initializationList:
+		for b in range(random.randint(0,3)):
+			rotate_tile_clockwise(a)
+	random.shuffle(initializationList)
+	for column in range(7):
+		for row in range(7):
+			#if [row][column] != t0 or [row][column] != t1 or [row][column] != t2 or [row][column] != t3 or [row][column] != t4 or [row][column] != t5 or [row][column] != t6 or [row][column] != t7 or [row][column] != t8 or [row][column] != t9 or [row][column] != t10 or [row][column] != t11 or [row][column] != t12 or [row][column] != t13 or [row][column] != t14 or [row][column] != t15:
+			if TILE_ARRAY[row][column] == None:#This is testing if TILE_ARRAY[row][column] is empty, may have to change logic to work
+				TILE_ARRAY[row][column] = initializationList[0]
+				TILE_ARRAY[row][column].currentrow = row
+				TILE_ARRAY[row][column].currentcolumn = column
+				del initializationList[0]
+
+	#print(TILE_ARRAY)#shows contents of tile_array
+	return TILE_ARRAY
+
+def find_path(tile, Array):
+
+	tile.travelable = 1
+
+	if tile.currentrow <=0:
+		NTile = None
+	else:
+		NTile = Array[((tile.currentrow)-1)][(tile.currentcolumn)]
+
+	if tile.currentrow >=6:
+		STile = None
+	else:
+		STile = Array[((tile.currentrow)+1)][(tile.currentcolumn)]
+
+	if tile.currentcolumn >=6:
+		ETile = None
+	else:
+		ETile = Array[(tile.currentrow)][((tile.currentcolumn)+1)]
+
+	if tile.currentcolumn <=0:
+		WTile = None
+	else:
+		WTile = Array[(tile.currentrow)][((tile.currentcolumn)-1)]
+
+	if tile.north is 1 and NTile is not None and NTile.south is 1 and NTile.travelable is 0:
+		#print("Tile Travelable South to North from", tile.currentrow, tile.currentcolumn, "to", NTile.currentrow, NTile.currentcolumn)
+		find_path(NTile, Array)
+
+	if tile.east is 1 and ETile is not None and ETile.west is 1 and ETile.travelable is 0:
+		#print("Tile Travelable West to East", tile.currentrow, tile.currentcolumn, "to", ETile.currentrow, ETile.currentcolumn)
+		find_path(ETile, Array)
+
+	if tile.south is 1 and STile is not None and STile.north is 1 and STile.travelable is 0:
+		#print("Tile Travelable North to South", tile.currentrow, tile.currentcolumn, "to", STile.currentrow, STile.currentcolumn)
+		find_path(STile, Array)
+
+	if tile.west is 1 and WTile is not None and WTile.east is 1 and WTile.travelable is 0:
+		#print("Tile Travelable East to West", tile.currentrow, tile.currentcolumn, "to", WTile.currentrow, WTile.currentcolumn)
+		find_path(WTile, Array)
 
 
-def grab_and_randomize_tiles():#Function which grabs tiles in specified directory and puts them in random positions in matrix
-	elbowTiles = []#Stores elbow tiles
-	straightTiles = []#Stores straight tiles 
-	tTiles = []#Stores T Tiles
-	randomImagePaths = []#Stores random rotated image paths
 
-	twoDimBoard = [[0 for x in range(7)] for x in range(7)]#Creates matrix to store tiles in random positions
-
-	os.chdir("..\LabyrinthProject")#Grabs image files that are from directory
-	for file in glob.glob("Elbow*"):
-		elbowTiles.append(file)
-	for file in glob.glob("Straight*"):
-		straightTiles.append(file)
-	for file in glob.glob("T*"):
-		tTiles.append(file)
-	for x in range(15):#Number of unfixed elbow tiles
-		randomImagePaths.append(random.choice(elbowTiles))
-	for x in range(13):#Number of unfixed elbow tiles
-		randomImagePaths.append(random.choice(straightTiles)) 
-	for x in range(6):#Number of unfixed elbow tiles
-		randomImagePaths.append(random.choice(tTiles))
-
-	for c in range(7):
-		for r in range(7):
-			currPos = tuple([c, r])
-			if currPos == (0, 1) or currPos == (0, 3) or currPos == (0, 5) or currPos == (1, 0) or currPos == (1, 1) or currPos == (1, 2) or currPos == (1, 3) or currPos == (1, 4) or currPos == (1, 5) or currPos == (1, 6) or currPos == (2, 1) or currPos == (2, 3) or currPos == (2, 5) or currPos == (3, 0) or currPos == (3, 1) or currPos == (3, 2) or currPos == (3, 3) or currPos == (3, 4) or currPos == (3, 5) or currPos == (3, 6) or currPos == (4, 1) or currPos == (4, 3) or currPos == (4, 5) or currPos == (5, 0) or currPos == (5, 1) or currPos == (5, 2) or currPos == (5, 3) or currPos == (5, 4) or currPos == (5, 5) or currPos == (5, 6) or currPos == (6, 1) or currPos == (6, 3) or currPos == (6, 5):#checks if current position is for non fixed tiles
-				imagePath = random.choice(randomImagePaths)
-				twoDimBoard[c][r] = imagePath
-				randomImagePaths.remove(imagePath)
-
-	return twoDimBoard
-
-def grab_fixed_tiles():#Function which grabs tiles in specified directory and puts them in fixed positions in matrix
-	elbowTiles = []#Stores elbow tiles
-	tTiles = []#Stores T Tiles
-
-	twoDimBoard = [[0 for x in range(7)] for x in range(7)]#Creates matrix to store tiles in fixed positions
-
-	os.chdir("..\LabyrinthProject")#Grabs image files that are from directory
-	for file in glob.glob("Starting*"):
-		twoDimBoard[0][0] = file#Matrix goes by row first and column second
-	for file in glob.glob("ElbowSouthWest*"):
-		twoDimBoard[6][0] = file
-	for file in glob.glob("ElbowWestNorth*"):
-		twoDimBoard[6][6] = file
-	for file in glob.glob("ElbowNorthEast*"):
-		twoDimBoard[0][6] = file
-	for file in glob.glob("TEastSouthWest*"):
-		twoDimBoard[2][0] = file
-		twoDimBoard[4][0] = file
-		twoDimBoard[4][2] = file
-	for file in glob.glob("TNorthEastSouth*"):
-		twoDimBoard[0][2] = file
-		twoDimBoard[2][2] = file
-		twoDimBoard[0][4] = file
-	for file in glob.glob("TNorthSouthWest*"):
-		twoDimBoard[6][2] = file
-		twoDimBoard[4][4] = file
-		twoDimBoard[6][4] = file
-	for file in glob.glob("TWestNorthEast*"):
-		twoDimBoard[2][4] = file
-		twoDimBoard[2][6] = file
-		twoDimBoard[4][6] = file
-
-	return twoDimBoard
+def color_untravelable_path(Array, display):
+	for x in range(7):
+		for y in range(7):
+			if Array[x][y].travelable is 0:
+				Array[x][y].image.fill((150, 0, 0, 255), special_flags=pygame.BLEND_RGB_MULT) #Color unmoveable path red
+				display.blit(Array[x][y].image, (get_tile_coordinates(y, x)))
 
 
-
-		
-
-
+#TILE_ARRAY[r][c].get_image_filepath()
+	#TILE_ARRAY[r][c].draw(screen, (get_tile_coordinates(c, r)))
 
 
-
-
-
-
-
-
-
-		
-
-
-
+def move_player(tile, Array, display):
+	if tile.travelable is 1:
+		for x in range(7):
+			for y in range(7):
+				Array[y][x].get_image_filepath()
+				Array[y][x].draw(display, (get_tile_coordinates(x, y)))
+				Array[y][x].travelable = 0
+				Array[y][x].p1 = 0
+		display.blit(pygame.image.load(r'LabyrinthPlayerOneT.png'), (get_tile_coordinates(tile.currentrow, tile.currentcolumn)))
+		tile.travelable = 1
+		tile.p1 = 1
--- Labyrinth_Title.py
+++ Labyrinth_Title.py
@@ -1,10 +1,13 @@
 import pygame 
 import sys
 import os
+import Input_Text_Box
 from enum import Enum
 from pygame.rect import Rect
 from Sprite import *
 from Labyrinth_Main import main_game
+from Labyrinth_Client import establish_connection
+
 
 
 #Global Colors used
@@ -15,6 +18,7 @@ BLACK = (0, 0, 0)
 #Init clock
 clock = pygame.time.Clock()
 
+        
 #Main function to track gamestate
 def main():
 	pygame.init()
@@ -44,6 +48,12 @@ def main():
 
 		if game_state == GameState.RULES:
 			game_state = rules_screen(screen)
+
+		if game_state == GameState.CONNECT:
+			game_state = connect_screen(screen)
+
+		if game_state == GameState.CONNECT_to_server:
+			game_state = establish_connection(ip_text_box.Fetch_string, port_text_box.Fetch_string)
 
 		if game_state == GameState.QUIT:
 			pygame.quit()
@@ -75,15 +85,25 @@ def title_screen(screen):
 
 	#Create Quit Game Button
 	quit_btn = UIElement(
-		center_position=(640, 650),
+		center_position=(640, 725),
 		font_size=30,
 		bg_rgb= BLACK,
 		text_rgb= WHITE,
 		text= "Quit",
 		action= GameState.QUIT,
 	)
+    
+	#Create Quit Game Button
+	connect_btn = UIElement(
+		center_position=(640, 650),
+		font_size=30,
+		bg_rgb= BLACK,
+		text_rgb= WHITE,
+		text= "Connect",
+		action= GameState.CONNECT,
+	)
 
-	buttons = [start_btn, quit_btn, rules_btn]
+	buttons = [start_btn, quit_btn, rules_btn, connect_btn]
 
 	#Main Title Screen Loop
 	while True:
@@ -119,7 +139,6 @@ def title_screen(screen):
 
 #Main function of Rules Screen
 def rules_screen(screen):
-
 	while True:
 
 		#Create Return Button
@@ -146,13 +165,118 @@ def rules_screen(screen):
 				pygame.quit()
 				sys.exit()
 
-		
-
 		ui_action = return_btn.update(pygame.mouse.get_pos(), mouse_up)
 		if ui_action is not None:
 			return ui_action
 		return_btn.draw(screen)
 
+		#Set to 60 FPS
+		clock.tick(60)
+		pygame.display.flip()
+        
+        
+def connect_screen(screen):
+	ip_text_box = Input_Text_Box.input_text_box(screen, 500, 500, 300, 30, WHITE)
+	port_text_box = Input_Text_Box.input_text_box(screen, 500, 450, 300, 30, WHITE)
+
+	while True:
+
+		#Create Return Button
+		return_btn = UIElement(
+		center_position=(640, 725),
+		font_size=30,
+		bg_rgb= BLACK,
+		text_rgb= WHITE,
+		text="Return to Main Menu",
+		action= GameState.TITLE,
+		)
+
+		connect_info = UIElement(
+		center_position=(640, 400),
+		font_size=30,
+		bg_rgb= BLACK,
+		text_rgb= WHITE,
+		text="Please Enter server IP address and Port:",
+		action= GameState.TITLE,
+		)
+
+		IP_info = UIElement(
+		center_position=(450, 450),
+		font_size=30,
+		bg_rgb= BLACK,
+		text_rgb= WHITE,
+		text="IP:",
+		action= GameState.TITLE,
+		)
+
+		PORT_info = UIElement(
+		center_position=(430, 510),
+		font_size=30,
+		bg_rgb= BLACK,
+		text_rgb= WHITE,
+		text="PORT:",
+		action= GameState.TITLE,
+		)
+        
+		IP_string = UIElement(
+		center_position=(650, 515),
+		font_size=30,
+		bg_rgb= WHITE,
+		text_rgb= BLACK,
+		text= ip_text_box.Fetch_string(),
+		action= GameState.TITLE,
+		)
+        
+		PORT_string = UIElement(
+		center_position=(650, 465),
+		font_size=30,
+		bg_rgb= WHITE,
+		text_rgb= BLACK,
+		text= port_text_box.Fetch_string(),
+		action= GameState.TITLE,
+		)
+        
+		connect_server = UIElement(
+		center_position=(650, 650),
+		font_size=30,
+		bg_rgb= BLACK,
+		text_rgb= WHITE,
+		text= "[Connect to Server]",
+		action= establish_connection(ip_text_box.Fetch_string, port_text_box.Fetch_string()),
+		)
+
+
+		#Set background color
+		screen.fill(BLACK)
+
+		mouse_up = False
+		for event in pygame.event.get():
+			if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+				mouse_up = True
+			elif event.type == pygame.QUIT:
+				pygame.quit()
+				sys.exit()
+
+		ip_text_box.Update()
+		port_text_box.Update()
+
+
+
+
+
+		ui_action = return_btn.update(pygame.mouse.get_pos(), mouse_up)
+		if ui_action is not None:
+			return ui_action
+		ui_action_two = connect_server.update(pygame.mouse.get_pos(), mouse_up)
+		if ui_action_two is not None:
+			return ui_action_two
+		return_btn.draw(screen)
+		connect_info.draw(screen)
+		IP_info.draw(screen)
+		PORT_info.draw(screen)
+		IP_string.draw(screen)
+		PORT_string.draw(screen)
+		connect_server.draw(screen)
 		#Set to 60 FPS
 		clock.tick(60)
 		pygame.display.flip()
@@ -164,6 +288,8 @@ class GameState(Enum) :
 	TITLE = 0
 	GAME = 1
 	RULES = 2
+	CONNECT = 3
+	CONNECT_to_server = 4
 
 if __name__ == "__main__":
 	main()
